d05569f62c1ea4af4c2c2e691617dbc8bcdd539e
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-13 12:00:05 +0300

    Pages/MaintenanceTypes created and  OnGet methods created.

--- CNCMaintenanceAutomation/Pages/MaintenanceTypes/Index.cshtml.cs
+++ CNCMaintenanceAutomation/Pages/MaintenanceTypes/Index.cshtml.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CNCMaintenanceAutomation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CNCMaintenanceAutomation.Pages.MaintenanceTypes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
         {
+            _applicationDbContext = applicationDbContext;
+        }
+        
+        public IList<MaintenanceType> MaintenanceType { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            MaintenanceType = await _applicationDbContext.MaintenanceTypes.ToListAsync();
+            return Page();
         }
     }
 }
