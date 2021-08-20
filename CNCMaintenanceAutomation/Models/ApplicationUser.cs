3a343fd6786836493e61719c3b11fae2c47d0fca
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-20 13:12:50 +0300

    List users

--- CNCMaintenanceAutomation/Models/ApplicationUser.cs
+++ CNCMaintenanceAutomation/Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,7 +18,7 @@ namespace CNCMaintenanceAutomation.Models
         public string Address { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
-        
+
 
 
     }
---
+++ CNCMaintenanceAutomation/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
+using CNCMaintenanceAutomation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNCMaintenanceAutomation.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<ApplicationUser> ApplicationUsersList { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ApplicationUsersList = await _context.ApplicationUsers.ToListAsync();
+            return Page();
+        }
+    }
+}
