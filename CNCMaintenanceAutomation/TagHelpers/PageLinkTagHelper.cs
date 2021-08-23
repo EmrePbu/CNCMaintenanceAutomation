06cdc80efb94eba0eac1e12819ab78393cfbb41b
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-23 14:42:38 +0300

    Page Link Helper and Custom Tag Helper

--- CNCMaintenanceAutomation/TagHelpers/PageLinkTagHelper.cs
+++ CNCMaintenanceAutomation/TagHelpers/PageLinkTagHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CNCMaintenanceAutomation.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -26,13 +27,30 @@ namespace CNCMaintenanceAutomation.TagHelpers
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
 
-        public PagingInfo pageModel { get; set; }
+        public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
         public string PageClassSelected { get; set; }
 
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //base.Process(context, output);
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder tagBuilder = new TagBuilder("div");
 
+            for (int i = 0; i < PageModel.TotalItems; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = PageModel.UrlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                tag.InnerHtml.Append(i.ToString());
+                tagBuilder.InnerHtml.AppendHtml(tag);
+            }
 
+            output.Content.AppendHtml(tagBuilder.InnerHtml);
+        }
     }
 }
