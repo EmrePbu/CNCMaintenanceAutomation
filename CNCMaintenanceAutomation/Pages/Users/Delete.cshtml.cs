0686468433873d4799a975a537210fc2c96d631e
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-26 11:08:16 +0300

    User delete methods created ( OnPost and OnGet ).

---
+++ CNCMaintenanceAutomation/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
+using CNCMaintenanceAutomation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNCMaintenanceAutomation.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _context;
+        
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+            ApplicationUser = await _context.ApplicationUsers.FirstOrDefaultAsync(model => model.Id == id);
+
+            if (ApplicationUser == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+
+            if (id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+            ApplicationUser = await _context.ApplicationUsers.FindAsync(id);
+            
+            if (ApplicationUser != null)
+            {
+                _context.ApplicationUsers.Remove(ApplicationUser);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
