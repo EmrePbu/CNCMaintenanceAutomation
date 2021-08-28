138ca461ba5a9cffe69f12321986a654476df87d
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-08-28 17:25:54 +0300

    UserMachineViewModel and index get methods created.

---
+++ CNCMaintenanceAutomation/Models/ViewModel/UserMachineViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CNCMaintenanceAutomation.Models.ViewModel
+{
+    public class UserMachineViewModel
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<CncMachine> Machines { get; set; }
+
+    }
+}
---
+++ CNCMaintenanceAutomation/Pages/Machines/Index.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
+using CNCMaintenanceAutomation.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNCMaintenanceAutomation.Pages.Machines
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        [BindProperty]
+        public UserMachineViewModel UserMachineViewModel { get; set;}
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string ownerId = null)
+        {
+            if (ownerId == null)
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                ownerId = claim.Value;
+                //return NotFound();
+            }
+
+            UserMachineViewModel = new UserMachineViewModel()
+            {
+                Machines = await _context.CncMachines.Where(a => a.OwnerId == ownerId).ToListAsync(),
+                ApplicationUser = await _context.ApplicationUsers.FirstOrDefaultAsync(a => a.Id == ownerId),
+            };
+
+            return Page();
+
+        }
+    }
+}
