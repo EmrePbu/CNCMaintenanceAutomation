78bdd3f57e36f12cfb11836b00cf156ab31b3077
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-09-01 10:36:44 +0300

    Created Maintenance Service Detail Model

---
+++ CNCMaintenanceAutomation/Models/MaintenanceServiceDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CNCMaintenanceAutomation.Models
+{
+    public class MaintenanceServiceDetail
+    {
+        public int Id { get; set; }
+
+        public int MaintenanceServiceGeneralId { get; set; }
+
+        [ForeignKey("MaintenanceServieGeneralId")]
+        public virtual MaintenanceServiceGeneral MaintenanceServiceGeneral { get; set; }
+
+        [Display(Name ="Maintenance")]
+        public int MaintenanceTypeId { get; set; }
+
+        [ForeignKey("MaintenanceId")]
+        public virtual MaintenanceType MaintenanceType { get; set; }
+    
+        public double MaintenancePrice { get; set; }
+    
+        public string MaintenanceName { get; set; }
+    }
+}
