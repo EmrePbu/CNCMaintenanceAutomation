62ccb937751773b1450b4605c829f401dd6a2928
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-09-01 19:09:42 +0300

    Created Maintenance.CreateModel OnPost methods

---
+++ CNCMaintenanceAutomation/Pages/Maintenances/Create.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
+using CNCMaintenanceAutomation.Models;
+using CNCMaintenanceAutomation.Models.ViewModel;
+using CNCMaintenanceAutomation.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNCMaintenanceAutomation.Pages.Maintenances
+{
+    [Authorize(Roles = StaticValues.AdminUser)]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Post metoduna bagliyoruz.
+        [BindProperty]
+        public CncMachineMaintenanceServiceViewModel CncMachineMaintenanceServiceViewModel { get; set; }
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Pages/Machines/Index.cshtml - Line 128 - router name
+        public async Task<IActionResult> OnGetAsync(int? machineId)
+        {
+            if (machineId == null)
+            {
+                return NotFound();
+            }
+
+            CncMachineMaintenanceServiceViewModel = new CncMachineMaintenanceServiceViewModel
+            {
+                CncMachine = await _context.CncMachines.Include(a => a.ApplicationUser).FirstOrDefaultAsync(a => a.Id == machineId),
+                MaintenanceServiceGeneral = new Models.MaintenanceServiceGeneral(),
+            };
+
+            List<String> MaintenanceServiceCardList = _context.MaintenanceServiceCards.Include(a => a.MaintenanceType).Where(a => a.MachineId == machineId).Select(a => a.MaintenanceType.MaintenanceName).ToList();
+
+            //C# 'de LINQ sorgularý yazma#
+            IQueryable<MaintenanceType> MaintenanceTypesList = from x in _context.MaintenanceTypes where !(MaintenanceServiceCardList.Contains(x.MaintenanceName)) select x;
+
+            CncMachineMaintenanceServiceViewModel.MaintenanceTypesList = MaintenanceTypesList.ToList();
+
+            CncMachineMaintenanceServiceViewModel.MaintenanceServiceCardsList = _context.MaintenanceServiceCards.Include(a => a.MaintenanceType).Where(a => a.MachineId == machineId).ToList();
+
+
+            CncMachineMaintenanceServiceViewModel.MaintenanceServiceGeneral.TotalPrice = 0;
+            foreach (var item in CncMachineMaintenanceServiceViewModel.MaintenanceServiceCardsList)
+            {
+                CncMachineMaintenanceServiceViewModel.MaintenanceServiceGeneral.TotalPrice += item.MaintenanceType.MaintenancePrice;
+            }
+
+            return Page();
+        }
+    }
+}
