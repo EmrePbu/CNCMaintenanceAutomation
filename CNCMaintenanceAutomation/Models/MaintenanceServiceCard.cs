d70596b955e5db6d2970d46e6a32f9722b655b0b
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-09-01 09:27:38 +0300

    Created MaintenanceServiceCard.

---
+++ CNCMaintenanceAutomation/Models/MaintenanceServiceCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CNCMaintenanceAutomation.Models
+{
+    public class MaintenanceServiceCard
+    {
+        public int Id { get; set; }
+        public int MachineId { get; set; }
+        public int MaintenanceTypeId { get; set; }
+
+        [ForeignKey("MachineId")]
+        public virtual CncMachine CncMachine { get; set; }
+        
+        [ForeignKey("MaintenanceTypeId")]
+        public virtual MaintenanceType MaintenanceType { get; set; }
+
+
+    }
+}
