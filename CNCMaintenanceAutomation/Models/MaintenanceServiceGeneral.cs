51f060a9c3185dc2d4ade65f76801a12cdd00fe3
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-09-04 14:46:53 +0300

    Added display name for MaintenanceServiceGeneral. Created Maintenance History page.

--- CNCMaintenanceAutomation/Models/MaintenanceServiceGeneral.cs
+++ CNCMaintenanceAutomation/Models/MaintenanceServiceGeneral.cs
@@ -11,21 +11,26 @@ namespace CNCMaintenanceAutomation.Models
     {
         public int Id { get; set; }
         
+        [Display(Name ="Machine Operation Time")]
         public double MachineOperationTime { get; set; }
         
         [Required]
+        [Display(Name ="Total Price")]
         public double TotalPrice { get; set; }
         
+        [Display(Name ="Details")]
         public string Details { get; set; }
 
         [Required]
-        [DisplayFormat(DataFormatString ="{0:dd/MMM/YYYY}")]
+        [DisplayFormat(DataFormatString ="{0:dd MMM yyyy}")]
+        [Display(Name ="Creation Date")]
         public DateTime CreationDate { get; set; }
 
         public int CncMachineId { get; set; }
 
         //Burada ki key onceden MachineId idi
         [ForeignKey("CncMachineId")]
+        [Display(Name ="Cnc Machine")]
         public virtual CncMachine CncMachine { get; set; }
 
     }
