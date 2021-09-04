a513bff5e0a6e239a48e98800d11a89c12d15bd1
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-09-04 15:13:50 +0300

    Created Maintenance Details page PageModel

---
+++ CNCMaintenanceAutomation/Pages/Maintenances/Details.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CNCMaintenanceAutomation.Data;
+using CNCMaintenanceAutomation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNCMaintenanceAutomation.Pages.Maintenances
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public MaintenanceServiceGeneral MaintenanceServiceGeneral { get; set; }
+        public List<MaintenanceServiceDetail> MaintenanceServiceDetails { get; set; }
+
+        public void OnGet(int? maintenanceId)
+        {
+
+            MaintenanceServiceGeneral = _context.MaintenanceServiceGenerals.Include(a => a.CncMachine).Include(a => a.CncMachine.ApplicationUser).FirstOrDefault(a => a.Id == maintenanceId);
+
+            MaintenanceServiceDetails = _context.MaintenanceServiceDetails.Where(a => a.MaintenanceServiceGeneralId == maintenanceId).ToList();
+        }
+    }
+}
